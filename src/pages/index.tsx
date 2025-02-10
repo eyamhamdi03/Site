741b75fff6b4d828c9a178637bec5148f48992d2
eyamhamdi03 <eyamhamdi2003@gmail.com>
2025-02-11 00:01:56 +0100

    tailwindcss and design system configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 
 import { Seo } from "@/ui/component/seo";
+import { Typography } from "@/ui/design-system/typography/typography";
 export default function Home() {
 return (
     <>
 <Seo title="Site" description="Description ..." />
-<h1>Hello World!</h1>   
+<Typography >Salut</Typography>
+<Typography theme="primary" >Salut</Typography>
+
     </>
    
 );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,166 @@
 import type { Config } from "tailwindcss";
-
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{html,js,jsx,ts,tsx}', // Scan all files in the src directory
   ],
   theme: {
+    fontSize : {
+     "8xl": [
+				"120px",
+				{
+					lineHeight: "120px",
+					letterSpacing: "-6px",
+					fontWeight: "500",
+				},
+			],
+			"7xl": [
+				"72px",
+				{
+					lineHeight: "80px",
+					letterSpacing: "-4.5px",
+					fontWeight: "600",
+				},
+			],
+			"6xl": [
+				"55px",
+				{
+					lineHeight: "60px",
+					letterSpacing: "-2.5px",
+					fontWeight: "500",
+				},
+			],
+			"5xl": [
+				"48px",
+				{
+					lineHeight: "54px",
+					letterSpacing: "-1.600000023841858px",
+					fontWeight: "500",
+				},
+			],
+			"4xl": [
+				"36px",
+				{
+					lineHeight: "44px",
+					letterSpacing: "-1.2000000476837158px",
+					fontWeight: "500",
+				},
+			],
+			"3xl": [
+				"28px",
+				{
+					lineHeight: "34px",
+					letterSpacing: "-0.800000011920929px",
+					fontWeight: "500",
+				},
+			],
+			"2xl": [
+				"24px",
+				{
+					lineHeight: "30px",
+					letterSpacing: "-1px",
+					fontWeight: "400",
+				},
+			],
+			xl: [
+				"24px",
+				{
+					lineHeight: "30px",
+					letterSpacing: "-1px",
+					fontWeight: "400",
+				},
+			],
+			lg: [
+				"21px",
+				{
+					lineHeight: "30px",
+					letterSpacing: "-0.800000011920929px",
+					fontWeight: "400",
+				},
+			],
+			base: [
+				"17px",
+				{
+					lineHeight: "25px",
+					letterSpacing: "-0.699999988079071px",
+					fontWeight: "400",
+				},
+			],
+			sm: [
+				"15px",
+				{
+					lineHeight: "23px",
+					letterSpacing: "-0.6000000238418579px",
+					fontWeight: "400",
+				},
+			],
+			caption1: [
+				"20px",
+				{
+					lineHeight: "24px",
+					letterSpacing: "-0.6000000238418579px",
+					fontWeight: "400",
+				},
+			],
+			caption2: [
+				"18px",
+				{
+					lineHeight: "20px",
+					letterSpacing: "-0.30000001192092896px",
+					fontWeight: "400",
+				},
+			],
+			caption3: [
+				"16px",
+				{
+					lineHeight: "18px",
+					letterSpacing: "-0.5px",
+					fontWeight: "400",
+				},
+			],
+			caption4: [
+				"13px",
+				{
+					lineHeight: "15px",
+					letterSpacing: "-0.20000000298023224px",
+					fontWeight: "400",
+				},
+			],
+    },
+    colors: {
+			white: "#ffffff",
+			primary: {
+				200: "#efeffc",
+				300: "#cfd0f6",
+				400: "#7f81e8",
+				DEFAULT: "#5F62E2",
+				600: "#5658CB",
+			},
+			secondary: {
+				200: "#eaf8f4",
+				300: "#bfe9de",
+				400: "#56c4a7",
+				DEFAULT: "#2AB691",
+				600: "#26a482",
+			},
+			gray: {
+				300: "#fafafa",
+				400: "#f2f2f2",
+				500: "#e5e5e5",
+				600: "#b2b2b2",
+				700: "#808080",
+				800: "#333333",
+				DEFAULT: "#1D1D1D",
+			},
+			alert: {
+				danger: "#FF4E4E",
+				success: "#90DA1",
+				warning: "#FEB72F",
+			},
+		},
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
   },
   plugins: [],
